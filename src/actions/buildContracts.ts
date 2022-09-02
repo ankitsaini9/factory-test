089440d97700204fa420339b76031e0d057f79ca
Sahil Tembhare <sahil@rapidinnovation.dev>
2022-09-02 16:31:45 +0530

    made changes according to the test contract and types workaround

--- src/actions/buildContracts.ts
+++ src/actions/buildContracts.ts
@@ -1,11 +1,12 @@
 import type { Config } from "../config";
 import { buildContract } from "./buildContract";
 import { buildTypes } from "./buildTypes";
+import * as fs from 'fs';
 
 export const buildContracts = async (config: Config) => {
     for (const { path } of config.contracts) {
         await buildContract(path);
     }
-
-    await buildTypes(config);
+    // await buildTypes(config);
+    // types are already generated
 }
